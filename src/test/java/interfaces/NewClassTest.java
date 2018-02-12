5fe4de040c797867708ce2c692d2d313b603cdd1
Justin Joseph <justin.joseph72@outlook.com>
2018-02-12 22:42:18 +0000

    sdf

---
+++ src/test/java/interfaces/NewClassTest.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import org.junit.Test;
+
+public class NewClassTest {
+
+    @Test
+    public void test1()
+    {
+        NewCalss newCalss = new NewCalss();
+        newCalss.test1();
+    }}
